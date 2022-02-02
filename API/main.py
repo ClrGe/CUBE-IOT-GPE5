29ee5cce84118a95ba4cc54ad195f3bc9f6b660f
alancesi <98748425+alancesi@users.noreply.github.com>
2022-02-02 10:53:21 +0100

    add CRUD for sensors

--- API/main.py
+++ API/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from pickle import TRUE
 from flask import Flask, request
 from flask_restx import Resource, Api, fields, reqparse
@@ -9,6 +10,7 @@ import json
 today = date.today()
 
 d1 = today.strftime("%d%m%Y")
+
 logging.basicConfig(filename='./logs/logs_'+d1+'.log', level=logging.INFO, format=f'%(asctime)s %(levelname)s : %(message)s')
 
 config = json.load(open("./configs.json"))
@@ -23,7 +25,67 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 app = Flask(__name__)
+
 api = Api(app=app, version="0.1", doc="/api", title="API station météo", description="API dédié au projet IOT cube", default="API", default_label='', validate=True)
+
+sensors_post_definition = api.model('sensors post informations', {
+    'latitude': fields.String(required=True),
+    'longitude': fields.String(required=True),
+    'active': fields.Boolean(required=True)
+})
+
+sensors_put_definition = api.model('sensors put informations', {
+    'latitude': fields.String(),
+    'longitude': fields.String(),
+    'active': fields.Boolean()
+})
+
+@api.route("/api/v1/sensors/<id>")
+class sonde(Resource):
+    @api.response(200, 'API sensors : Success')
+    def get(self, id):
+        """
+        Recherche d'un élément de la table id
+        """
+        if id:
+          mycursor.execute("SELECT * FROM sensors WHERE `sensorID` = '" + id + "';")
+          myresult = mycursor.fetchall()
+          print(myresult)
+          jso = json.dumps(myresult, indent=4, sort_keys=True, default=str)
+          return json.loads(jso), 200
+        else:
+          return {"error" : "missing arguments."}, 400 
+    @api.expect(sensors_put_definition)
+    def put(self, id):
+      """
+      Update dans la table sensors
+      """
+      if id:
+        request_data = request.get_json()
+        at_change = ""
+        for element in request_data:
+          if element == "latitude" or element == "longitude" or element == "active":
+            if at_change == "":
+              at_change = at_change + element + "='" + request_data[element] + "'"
+            else:
+              at_change = at_change + "," + element + "='" + request_data[element] + "'" 
+            
+        if at_change != "":
+          mycursor.execute("UPDATE sensors SET " + at_change + " WHERE sensorID = " + id +";")
+          mydb.commit()
+          return {"statut":"success"}, 200
+      else:
+        return {"error" : "missing arguments."}, 400
+    def delete(self, id):
+      """
+      Update dans la table sensors
+      """
+      if id:
+        mycursor.execute("DELETE FROM `sensors` WHERE sensorID = " + id +";")
+        mydb.commit()
+        return {"statut":"success"}, 200
+      else:
+        return {"error" : "missing arguments."}, 400
 
 @api.route("/api/v1/sensors")
 class sonde(Resource):
@@ -33,12 +95,12 @@ class sonde(Resource):
         Récupération de la table sensors
         """
         mycursor.execute("SELECT * FROM sensors")
-
         myresult = mycursor.fetchall()
 
         jso = json.dumps(myresult, indent=4, sort_keys=True, default=str)
         return json.loads(jso), 200
 
+    @api.expect(sensors_post_definition)
     def post(self):
         """
         Ajout dans la table sensors
@@ -46,12 +108,15 @@ class sonde(Resource):
         request_data = request.get_json()
 
         if request_data["latitude"] != None and request_data["longitude"] != None and request_data["active"] != None:
-            mycursor.execute("INSERT INTO `authors` (`latitude`, `longitude`, `active`) VALUES ('"+request_data["latitude"]+"', '"+request_data["longitude"]+"', '"+request_data["active"])
-    def put(self):
-      """
-      Update dans la table sensors
-      """
-        #return json.loads(jso), 200
+            if request_data["active"] == TRUE:
+              request_data["active"] = 1
+            else:
+              request_data["active"] = 0
+            mycursor.execute("INSERT INTO `sensors` (`latitude`, `longitude`, `active`) VALUES ('"+ str(request_data["latitude"])+"', '"+str(request_data["longitude"])+"', '"+str(request_data["active"]) + "');")
+            mydb.commit()
+            return {"statut":"success"}, 200
+        else:
+          return {"error" : "missing arguments."}, 400
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='5010', debug=True)
+    app.run(host='192.168.95.217', port='5010', debug=True)
