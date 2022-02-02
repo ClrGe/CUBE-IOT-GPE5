cb78ad0636bd3f686d03a930c18530e157f9032a
ClG-DI21 <98747640+ClG-DI21@users.noreply.github.com>
2022-02-02 13:48:28 +0100

    Correct connection issues + merge versions

--- Arduino/WifiAndSensor.ino
+++ Arduino/WifiAndSensor.ino
@@ -1,53 +1,54 @@
-/* AUTHORS: itsy-pandacrp, ClG-DI21
- *
- * Station météo Groupe 5
- * 
- * Pinout:
- * LCD device:
- * GND => GND
- * VCC => 5V
- * SDA => 11 (SDA)
- * SCL => 12 (SCL)
- * 
- * DHT device:
- * SOURCE => 3
- * GND => GND
- * VCC => 5V
- * 
-*/
+// HttpClient - Version: Latest 
+#include <HttpClient.h>
+#include <b64.h>
+
 #include <WiFiNINA.h>
+#include "defines.h"
+#include <Adafruit_Sensor.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <SPI.h>
 
 #define DHTPIN 3
 #define DHTTYPE DHT11
 
-/* WIFI */
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_SSID;
+float temp[5] = {0, 0, 0, 0, 0};
+float hum[5] = {0, 0, 0, 0, 0};
+uint32_t delayMS;
+
+// SSID ET PASSWORD À METTRE DANS ONGLET SECRETS (VOIR DOC WIFIINA)
+char ssid[] =  SECRET_SSID;
+char pass[] =  SECRET_PASS;
 int status = WL_IDLE_STATUS;
 
 int counter = 0;
-const server = "https://api.groupe5.fr";
-float temp[5] = {0,0,0,0,0};
-float hum[5] = {0,0,0,0,0};
+IPAddress server(192,168,95,217);
 
 WiFiSSLClient client;
-DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x3F,20,4);
+DHT dht(DHTPIN, DHTTYPE);
+
 
 void setup() {
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
+
   dht.begin();
+
   while (status != WL_CONNECTED) {
-    lcd.print("Connecting to network");
+    Serial.print("Tentative de connexion au réseau: ");
     Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
     delay(10000);
   }
+  Serial.println("Connecté au réseau !");
+  Serial.println("----------------------------------------");
+
+  while (!Serial);
+  
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Connected to network");
@@ -55,12 +56,54 @@ void setup() {
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("IP: " + WiFi.localIP());
-  Serial.begin(9600);
+
+  printData();
+
+  delay(50);
 }
 
-
 void loop() {
-  temp[4] = dht.readTemperature();
+  getSensorData();
+  while (client.available())
+  {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected())
+  {
+    Serial.println(F("\nDisconnecting from server."));
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+  delay(2000);
+
+}
+
+void printData() {
+
+  Serial.println("Board Information:");
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP : ");
+  Serial.println(ip);
+  Serial.println();
+  Serial.println("Network Information:");
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void getSensorData() {
+    temp[4] = dht.readTemperature();
   hum[4] = dht.readHumidity();
   lcd.setCursor(0,1);
   if ((counter/2) % 2){
@@ -85,7 +128,7 @@ void loop() {
     Serial.println("Hum= " + String(avgHum/5,1));
     Serial.println("Tem= " + String(avgTemp/5,1));
     Serial.println(server+"/upload?temp="+String(avgTemp/5,1)+"&hum="+String(avgHum/5,1));
-    client.connect(server+"/upload?temp="+String(avgTemp/5,1)+"&hum="+String(avgHum/5,1), 5010);
+    
+    //client.connect(server+"/upload?temp="+String(avgTemp/5,1)+"&hum="+String(avgHum/5,1), 5010);
   }
-  delay(1000);
 }
